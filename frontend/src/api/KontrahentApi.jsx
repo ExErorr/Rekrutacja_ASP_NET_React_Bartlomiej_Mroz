f013b2b5249432ea71be1d048fd26ffc7efc7309
ExErorr <kukuryk098@gmail.com>
2025-07-04 18:02:22 +0200

    add env example for api calls

--- frontend/src/api/KontrahentApi.jsx
+++ frontend/src/api/KontrahentApi.jsx
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.example.VITE_API_URL;
 
 export const getKontrahenci = async () =>
   await fetch(`${API_URL}/kontrahenci`).then((res) => res.json());
